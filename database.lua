3f21631e0961a4df727d9c877124e6917552232f
ONELua Team <oneluaproject@gmail.com>
2021-02-23 22:07:52 -0600

    Add files via upload

---
+++ database.lua
@@ -0,0 +1,9 @@
+Ports = {
+	{ id = 589, datap = "gta3", plugin = "fd_fix.skprx", type = "mobile" },
+	{ id = 612, datap = "maxpayne", plugin = "kubridge.skprx", type = "mobile" },
+	{ id = 616, datap = "gtavc", type = "PC" },
+	{ id = 618, datap = "gtasa", plugin = "kubridge.skprx", type = "mobile" },
+	{ id = 604, fdata = "SonicCD", type = "mobile" },
+	{ id = 607, fdata = "Sonic1", type = "mobile" },
+	{ id = 608, fdata = "Sonic2", type = "mobile" },
+}
