750ef6639c281f11288848d9859bf743c3c08e16
ONELua Team <oneluaproject@gmail.com>
2022-08-28 11:34:12 -0500

    Add files via upload

--- AUTOPLUG2/plugins/plugins.lua
+++ AUTOPLUG2/plugins/plugins.lua
@@ -115,7 +115,7 @@ plugins = {
 --with config&configpath
 { name = "TransparentImpose v1.0 by GrapheneCt", id = "TRANSIMPOSE.png", path = "TrImpose.suprx", section = "main", config = "trimpose.txt", configpath = "ur0:data/", version = "0001", desc = LANGUAGE["INSTALLP_DESC_TRIMPOSE"], KEY = "INSTALLP_DESC_TRIMPOSE", link = "https://raw.githubusercontent.com/GrapheneCt/TransparentImpose/master/README.md", },
 { name = "QuickLauncher by Ibrahim778 v1.2 (QuickMenuReborn)", id = "QUICKLAUNCHER.png", path = "QuickMenuReborn.suprx", section = "main", version = "0001", config = "QuickMenuReborn.zip", configpath = "ur0:/", dl = "QuickMenuReborn.zip", desc = LANGUAGE["INSTALLP_DESC_QUICKLAUNCHER"], KEY = "INSTALLP_DESC_QUICKLAUNCHER", link = "https://raw.githubusercontent.com/Ibrahim778/QuickLauncher/master/README.md", },
-{ name = "psp2wpp v1.0 by Princess of Sleeping", id = "PSP2WPP.png", path = "psp2wpp.suprx", section = "main", config = "waveparam.txt", configpath = "ux0:data/", version = "0001", desc = "OLAs", KEY = "OLAs", dl = "waveparam.bin", link = "https://raw.githubusercontent.com/Princess-of-Sleeping/psp2wpp/master/README.md", },
+{ name = "psp2wpp v1.0 by Princess of Sleeping", id = "PSP2WPP.png", path = "psp2wpp.suprx", section = "main", config = "waveparam.txt", configpath = "ux0:data/", version = "0001", desc = LANGUAGE["INSTALLP_DESC_WAVE"], KEY = "INSTALLP_DESC_WAVE", dl = "waveparam.bin", link = "https://raw.githubusercontent.com/Princess-of-Sleeping/psp2wpp/master/README.md", },
 --with vpk
 { name = "ftpeverywhere v1.0 by teakhanirons", id = "FTPEVERYWHERE.png", path = "ftpeverywhere.suprx", section = "main", version = "0001", desc = LANGUAGE["INSTALLP_DESC_FTPEVERYWHERE"], KEY = "INSTALLP_DESC_FTPEVERYWHERE", link = "https://raw.githubusercontent.com/teakhanirons/ftpeverywhere/master/README.md", },
 --2 prxs
@@ -139,6 +139,7 @@ plugins = {
 { name = "ShowInfo Lite by Yoti v0.2", id = "SHOWINFOLITE.png", path = "ShowInfoLite.suprx", section = "ALL", version = "0001", desc = LANGUAGE["INSTALLP_DESC_SHOWINFO"], KEY = "INSTALLP_DESC_SHOWINFO", },
 { name = "ShowInfo Full by Yoti v0.6", id = "SHOWINFOFULL.png", path = "ShowInfo.suprx", section = "ALL", version = "0001", desc = LANGUAGE["INSTALLP_DESC_SHOWINFOFU"], KEY = "INSTALLP_DESC_SHOWINFOFU", },
 { name = "Volume Control Pack by GrapheneCt v1.0", id = "VOLUMECTRL.png", path = "ModalVol.suprx", section = "ALL", version = "0001", desc = LANGUAGE["INSTALLP_DESC_MODALVOL"], KEY = "INSTALLP_DESC_MODALVOL", link = "https://raw.githubusercontent.com/GrapheneCt/Volume-Control-Pack/master/README.md", },
+{ name = "hide-autoplugin by SilicaAndPina v1.0", id = "HIDEAP.png", path = "hide-autoplugin.suprx", section = "ALL", version = "0001", desc = LANGUAGE["INSTALLP_DESC_HIDEAP"], KEY = "INSTALLP_DESC_HIDEAP", link = "https://raw.githubusercontent.com/KuromeSan/ScoreHaxhide-autoplugin/master/README.md", },
 
 --2 prxs
 { name = "MakePsmGreatAgain by SilicaAndPina v1.5", id = "MAKEPSM.png", path = "MakePsmGreatAgain.suprx", section = "PCSI00007", path2 = "MakePsmGreatAgain.suprx", section2 = "PCSI00009", version = "0003", desc = LANGUAGE["INSTALLP_DESC_MAKEPSM"], KEY = "INSTALLP_DESC_MAKEPSM", link = "https://silica.codes/SilicaAndPina/makepsmgreatagain/raw/branch/master/README.md", },
