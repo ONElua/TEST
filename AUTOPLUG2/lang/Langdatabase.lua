e2b438f7966a2c3107229bd3bec77d6fdd91f2e9
ONELua Team <oneluaproject@gmail.com>
2022-09-16 16:23:06 -0500

    Add files via upload

--- AUTOPLUG2/lang/Langdatabase.lua
+++ AUTOPLUG2/lang/Langdatabase.lua
@@ -4,7 +4,7 @@ Langs = {
 { id =  "JAPANESE",		version = "01.46",		author =  "Kouchan" },
 { id =  "SPANISH",		version = "01.57",		author =  "tetsuoScene" },
 { id =  "CHINESE_S",	version = "01.63",		author =  "yexun1995" },
-{ id =  "FRENCH",		version = "01.63",		author =  "Chronoss09" },
+{ id =  "FRENCH",		version = "01.65",		author =  "Chronoss09" },
 { id =  "GERMAN",		version = "01.45",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.65",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.46",		author =  "KierownikStacji" },
